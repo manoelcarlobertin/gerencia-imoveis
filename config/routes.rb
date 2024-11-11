3cb69fa6caa9d4e956383cd925bae42aa30d89e5
Manoel Carlo Bertin <manoelcarlobertin@outlook.com>
2024-11-11 20:16:20 -0300

    project created

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  devise_for :users
+
+  resource :dashboard, only: [ :show ]
+
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Render dynamic PWA files from app/views/pwa/*
+  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+
+  # Defines the root path route ("/")
+  root "dashboards#go_to_home"
+end
